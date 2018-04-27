dd8471dbc5cfefff4b9d849c9a26c29e17b59d49
Deliman206 <smiller206@me.com>
2018-04-27 11:18:37 -0700

    added some test material to binary search

--- binary-search/lib/binary-search.js
+++ binary-search/lib/binary-search.js
@@ -19,17 +19,27 @@ binarySearch.binary = (array, value) => {
   let min = 0;
   let max = array.length - 1;
   let mid = Math.floor((min + max) / 2);
-  
+  let count = 0;
   while (min <= max) {
     mid = Math.floor((max + min) / 2);
   
     if (array[mid] === value) {
-      return mid;
+      return count;
     } else if (array[mid] < value) {
       min = mid + 1;
     } else {
       max = mid - 1;
     }
+    count += 1;
   }
   return -1;
 };
+let count = 0;
+const testArray = [];
+while (count < 1000000) {
+  const num = Math.floor(Math.random() * 10000);
+  testArray.push(num);
+  count += 1;
+}
+testArray.sort((a, b) => a - b);
+console.log(binarySearch.binary(testArray, Math.floor(Math.random() * 10000)));
