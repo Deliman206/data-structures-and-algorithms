207f7a42cdf9fc10b00888372e091f61bcd619dd
Deliman206 <smiller206@me.com>
2018-05-02 16:14:20 -0700

    wrote code and tests

---
+++ matching-brackets/__test/matching-brackets.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import match from '../lib/matching-bracket';
+
+describe('Should validate the matching-bracket function', () => {
+  test('Should return true from valid string', () => {
+    const test0 = '(){}[]';
+    const test1 = '({}[])';
+    const test2 = '(){[]}';
+    expect(match(test0)).toBeTruthy();
+    expect(match(test1)).toBeTruthy();
+    expect(match(test2)).toBeTruthy();
+  });
+  test('Should return false from valid string', () => {
+    const test0 = '(){[}]';
+    const test1 = '({}[)])';
+    const test2 = '({})}{[]}';
+    expect(match(test0)).toBeFalsy();
+    expect(match(test1)).toBeFalsy();
+    expect(match(test2)).toBeFalsy();
+  });
+});
