198340d8aa0bedd6a51ce3c9e0f3797889be0b03
Deliman206 <smiller206@me.com>
2018-06-12 09:26:53 -0700

    fixed quick sort

--- quick-sort/__test__/quickSort.test.js
+++ quick-sort/__test__/quickSort.test.js
@@ -3,6 +3,6 @@ import quickSort from '../lib/quickSort';
 describe('Should Validate the quickSort function', () => {
   test('Should return array in sorted order', () => {
     const array = [3, 5, 1, 8, 6, 2];
-    expect(quickSort(array)).toEqual([1, 2, 3, 5, 6, 8]);
+    expect(quickSort(array, 0, array.length - 1)).toEqual([1, 2, 3, 5, 6, 8]);
   });
 });
--- quick-sort/lib/quickSort.js
+++ quick-sort/lib/quickSort.js
@@ -1,41 +1,42 @@
-function swap(array, li, ri) {
-  const temp = array[li];
-  array[li] = array[ri];
-  array[ri] = temp;
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 }
 
 function partitionHoare(array, left, right) {
-  let li = left;
-  let ri = right;
-  const pivot = Math.floor((li + ri) / 2);
-  while (li <= ri) {
-    while (array[li] < array[pivot]) {
-      li += 1;
+  const pivot = Math.floor((left + right) / 2);
+
+  while (left < right) {
+    while (array[left] < array[pivot]) {
+      left += 1;
     }
-    while (array[ri] > array[pivot]) {
-      ri -= 1;
+    while (array[right] > array[pivot]) {
+      right -= 1;
     }
-    if (li <= ri) {
-      swap(array, li, ri);
-      li += 1;
-      ri -= 1;
+
+    if (left <= right) {
+      swap(array, left, right);
+      left += 1;
+      right -= 1;
     }
   }
   return left;
 }
 
-export default function quicksort(array, left, right) {
-  const li = left || 0;
-  const ri = right || array.length - 1;
+export default function quicksortHoare(array, left, right) {
+  left = left || 0;
+  right = right || array.length - 1;
 
-  // var pivot = partitionLomuto(array, left, right); 
-  const pivot = partitionHoare(array, li, ri); 
+  const pivot = partitionHoare(array, left, right);
 
-  if (li < pivot - 1) {
-    quicksort(array, li, pivot - 1);
+  if (left < pivot - 1) {
+    quicksortHoare(array, left, pivot - 1);
   }
-  if (ri > pivot) {
-    quicksort(array, pivot, ri);
+
+  if (right > pivot) {
+    quicksortHoare(array, pivot, right)
   }
+
   return array;
 }
